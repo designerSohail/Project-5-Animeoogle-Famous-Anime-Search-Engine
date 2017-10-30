587808ed4c231809bc6b2a1496d81d7527759ff9
designerSohail <designersohail786@gmail.com>
2017-10-30 23:43:26 +0545

    Required files and home page added

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Animeoogle</title>
+    <link href="https://fonts.googleapis.com/css?family=Metal+Mania|Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <center>
+        <h1>Animeoogle</h1>
+        <div class="box">
+          <form action="#" method="post">
+            <input type="text" name="name" placeholder="Enter anime name">
+          </form>
+        </div>
+      </center>
+    </div>
+  </body>
+</html>
