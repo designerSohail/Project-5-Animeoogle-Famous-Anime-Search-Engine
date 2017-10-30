1dbeaf5cd4ffd839e0a441d644298921cafa4db0
designerSohail <designersohail786@gmail.com>
2017-10-31 00:11:31 +0545

    Home Page modifid with random color logo

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+  $colors = array(
+    '#1abc9c',
+    '#2ecc71',
+    '#3498db',
+    '#9b59b6',
+    '#34495e',
+    '#f1c40f',
+    '#e67e22',
+    '#e74c3c',
+    '#7f8c8d'
+  );
+  $web_name = 'Animeoogle';
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -9,9 +23,17 @@
   <body>
     <div class="wrapper">
       <center>
-        <h1>Animeoogle</h1>
+        <h1>
+          <?php
+            for ($i=0; $i < strlen($web_name); $i++) {
+          ?>
+            <span style="color:<?php echo $colors[mt_rand(0, 8)]; ?>"><?php echo $web_name[$i] ?></span>
+          <?php
+            }
+          ?>
+        </h1>
         <div class="box">
-          <form action="#" method="post">
+          <form action="output.php" method="post">
             <input type="text" name="name" placeholder="Enter anime name">
           </form>
         </div>
